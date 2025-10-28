3451ede95aa71497be84c1685b6832320c8e296b
gmp007 <cekuma1@gmail.com>
2025-10-28 13:05:05 -0400

    Remove some unnecessary folders

--- build/lib/lindhardkit/__init__.py
+++
@@ -1,7 +0,0 @@
-# lindhardkit/__init__.py
-__version__ = "0.1.0"
-
-from .state import STATE, RuntimeState  # lightweight and safe; remove if it causes heavy imports
-
-__all__ = ["STATE", "RuntimeState", "__version__"]
-
